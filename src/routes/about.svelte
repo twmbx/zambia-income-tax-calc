67159798fb24249f2e72fb68f4a7e1485bed0c92
Twaambo Haamucenje <twaambo@outlook.com>
2020-04-18 16:18:21 +0200

    update the about page.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,6 +4,7 @@
 
 <h1>About this tool</h1>
 
-<p>This tool calculates income after Zambian Tax.</p>
+<p>This tool calculates income after Zambian PAYE Tax and mandatory deductions.</p>
 <p>It is not to be considered sound legal advice or information.</p>
-<p>&copy;2019 <a href="https://twaambo.com">Twaambo Haamucenje</a></p>
+<p>Thanks <a href="https://github.com/k9uma">Mukuma Musenge</a> for contributing.</p>
+<p>&copy;2020 <a href="https://twaambo.com">Twaambo Haamucenje</a></p>
