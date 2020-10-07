81e20ca3e6ba334bad3b43189535b5e926451834
Twaambo Haamucenje <twaambo@outlook.com>
2020-10-07 17:27:09 +0200

    fix service worker

--- src/client.js
+++ src/client.js
@@ -3,3 +3,12 @@ import * as sapper from '@sapper/app';
 sapper.start({
 	target: document.querySelector('#sapper')
 });
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('service-worker.js')
+			.then(reg => console.log('Service Worker: Registered'))
+			.catch(err => console.log(`Service Worker: Error: ${err}`));
+	});
+}
