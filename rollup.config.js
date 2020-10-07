20cf15a67a3aad8e6590f2d92b691682111263a8
Twaambo Haamucenje <twaambo@outlook.com>
2020-10-07 17:26:50 +0200

    upgrade deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
-import json from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
@@ -11,6 +10,11 @@ import pkg from './package.json';
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) =>
+	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+	onwarn(warning);
 
 export default {
 	client: {
@@ -26,12 +30,15 @@ export default {
 				hydratable: true,
 				emitCss: true
 			}),
-			resolve(),
+			resolve({
+				browser: true,
+				dedupe: ['svelte']
+			}),
 			commonjs(),
 
 			legacy && babel({
 				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				runtimeHelpers: true,
+				babelHelpers: 'runtime',
 				exclude: ['node_modules/@babel/**'],
 				presets: [
 					['@babel/preset-env', {
@@ -50,6 +57,9 @@ export default {
 				module: true
 			})
 		],
+
+		preserveEntrySignatures: false,
+		onwarn,
 	},
 
 	server: {
@@ -60,17 +70,20 @@ export default {
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
-			json(),
 			svelte({
 				generate: 'ssr',
+				hydratable: true,
 				dev
 			}),
-			resolve(),
+			resolve({
+				dedupe: ['svelte']
+			}),
 			commonjs()
 		],
-		external: [].concat(
-			require('module').builtinModules || Object.keys(process.binding('natives'))
-		),
+		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+
+		preserveEntrySignatures: 'strict',
+		onwarn,
 	},
 
 	serviceworker: {
@@ -84,6 +97,9 @@ export default {
 			}),
 			commonjs(),
 			!dev && terser()
-		]
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
 	}
 };
--- src/server.js
+++ src/server.js
@@ -6,17 +6,12 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = polka() // You can also use Express
+polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-
-export default app.handler
-
-if (!process.env.NOW_REGION) {
-	app.listen(PORT, err => {
-		if (err) console.log('error', err)
-	})
-}
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
